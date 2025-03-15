d4ec603e8ed7be5a949b778ac31d10e6856f9d89
BrahyanPro <brahyanpro16@gmail.com>
2025-03-15 06:15:39 -0400

    FF-4 Feat: Agregar hook useMovieSuggestions para obtener películas similares y recomendadas

---
+++ hooks/useMovieSuggestions.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { getSimilarMovies, getRecommendedMovies } from '@/lib/movieApi';
+import { Movie } from '@/types/Movies';
+
+export const useMovieSuggestions = (movieId: number) => {
+  const { data: similarMovies, isLoading: loadingSimilar } = useQuery<Movie[]>({
+    queryKey: ['similarMovies', movieId],
+    queryFn: () => getSimilarMovies(movieId)
+  });
+
+  const { data: recommendedMovies, isLoading: loadingRecommended } = useQuery<Movie[]>({
+    queryKey: ['recommendedMovies', movieId],
+    queryFn: () => getRecommendedMovies(movieId)
+  });
+
+  return {
+    similarMovies,
+    recommendedMovies,
+    isLoading: loadingSimilar || loadingRecommended
+  };
+};

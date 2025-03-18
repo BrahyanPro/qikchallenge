3b3863ca1613ad9c956a37b0c840178e523d2c6e
BrahyanPro <brahyanpro16@gmail.com>
2025-03-18 04:17:00 -0400

    Mejoras Refactorizar tipado en hook useMovieSuggestions

--- hooks/useMovieSuggestions.ts
+++ hooks/useMovieSuggestions.ts
@@ -2,7 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getSimilarMovies, getRecommendedMovies } from '@/lib/movieApi';
 import { Movie } from '@/types/Movies';
 
-export const useMovieSuggestions = (movieId: number) => {
+export const useMovieSuggestions = (movieId: number | undefined) => {
+  if (!movieId) {
+    return {
+      similarMovies: undefined,
+      recommendedMovies: undefined,
+      isLoading: false
+    };
+  }
+
   const { data: similarMovies, isLoading: loadingSimilar } = useQuery<Movie[]>({
     queryKey: ['similarMovies', movieId],
     queryFn: () => getSimilarMovies(movieId)

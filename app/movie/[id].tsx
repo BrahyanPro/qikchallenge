d1f3714d8c4a259c65a9ecb24089c307a2283ccd
BrahyanPro <brahyanpro16@gmail.com>
2025-03-18 04:16:07 -0400

    Mejoras: Refactorizar componente MovieDetailScreen y agregar manejo de errores y carga

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,12 +1,27 @@
 import { useLocalSearchParams } from 'expo-router';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useMovieDetails } from '@/hooks/useMovieDetails';
 import MovieDetail from '@/components/MovieDetail';
+import SkeletonLoader from '@/components/ui/SkeletonLoader';
 
 export default function MovieDetailScreen() {
   const { id } = useLocalSearchParams();
   const movieId = Number(id);
+
+  if (isNaN(movieId)) {
+    return (
+      <SafeAreaView className='flex-1 bg-gray-900 p-4'>
+        <Text className='text-white'>Error: ID de película no válido</Text>
+      </SafeAreaView>
+    );
+  }
+
   const { data: movie, isLoading } = useMovieDetails(movieId);
+
+  if (isLoading) {
+    return <SkeletonLoader />;
+  }
 
   return (
     <SafeAreaView className='flex-1 bg-gray-900'>
